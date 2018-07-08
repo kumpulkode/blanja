ce550e1ad84958556eb9c23ad1ded57294a1af5d
candra <db.candra@gmail.com>
2018-07-08 23:19:39 +0700

    update readme.md
    update readme.md

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -347,14 +347,38 @@
     </script>
 
     <script type="text/javascript">
-        // Defaults override - hide "close all" button
-    $.jGrowl.defaults.closer = false;
-    // Solid color theme
-    $('.jgrowl-error').on('click', function () {
-        $.jGrowl('The Page you want access still under consttruction.', {
-            header: 'Sorry!',
+        $(function(){
+            // Sets the number of stars we wish to display
+            const numStars = 1000;
+            
+            // For every star we want to display
+            for (let i = 0; i < numStars; i++) {
+                let star = document.createElement("div");  
+                star.className = "star";
+                var xy = getRandomPosition();
+                    star.style.top = xy[0] + 'px';
+                    star.style.left = xy[1] + 'px';
+                $('#sw-modal-content').append(star);
+            }
+
+            // Gets random x, y values based on the size of the container
+            function getRandomPosition() {  
+                var y = window.innerWidth;
+                var x = window.innerHeight;
+                    var randomX = Math.floor(Math.random()*x);
+                    var randomY = Math.floor(Math.random()*y);
+                return [randomX,randomY];
+            }
+
+            // Defaults override - hide "close all" button
+            $.jGrowl.defaults.closer = false;
+            // Solid color theme
+            $('.jgrowl-error').on('click', function () {
+                $.jGrowl('The Page you want access still under consttruction.', {
+                    header: 'Sorry!',
+                });
+            });
         });
-    });
     </script>
 @endsection
 
